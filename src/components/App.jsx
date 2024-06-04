4d772445b140e56c61291d8b4b06babdc752794a
Dawid Bazyluk <dawid.bazyluk@gmail.com>
2024-06-04 04:18:15 +0200

    copy repo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,25 @@
+import Profile from "./Profile";
+import Statistics from "./Statistics";
+import FriendList from "./FriendList";
+import TransactionHistory from "./TransactionHistory";
+import user from "../data/user.json";
+import data from "../data/data.json";
+import friends from "../data/friends.json";
+import transactions from "../data/transactions.json";
+
 function App() {
   return (
     <>
-      <h1>React Homework Template (Vite)</h1>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
     </>
   );
 }
